988395f03e53dfc13e58a916bf3a99a2fd1aad9d
Renato Assis <renatoweverton@gmail.com>
2018-03-26 00:28:00 -0300

    coletando tweets automaticos

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,25 @@
 import { TwitterStream } from "./api/twitter";
 import { Telegram } from "./api/sprinterbot";
 import { Firebase } from "./api/firebase";
+import 'rxjs/add/operator/filter';
 require("dotenv").config();
-
 
 const firebase = new Firebase();
 const telegram = new Telegram(firebase);
 const twitter = new TwitterStream().getTweet$();
 
+const tweetSub = twitter.filter(tweet => {
+  if (!tweet.hasOwnProperty("retweeted_status")) {
+    return tweet;
+  }
+});
 
-/*twitter
-  .filter(tweet => {
-    if (!tweet.hasOwnProperty("retweeted_status")) {
-      return tweet;
-    }
-  })
-  .subscribe(tweet => {
-    hash.push(tweet.id)
-    telegram.setData(hash)  
-    firebase.set({
-      id: tweet.id,
-      text: tweet.text,
-      created_at: tweet.created_at,
-      status_url: `https://twitter.com/${this.screen_name}/status/${tweet.id}`
-    });
-  });*/
- telegram.run()
+tweetSub.subscribe(tweet => {
+   firebase.set({
+    id: tweet.id,
+    text: tweet.text,
+    created_at: tweet.created_at,
+    status_url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id}`
+  });
+});
+telegram.run();
